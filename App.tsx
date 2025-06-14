1288654a920e8c04908b763c81b3239d9225b652
uday5t61 <uday5t61@gmail.com>
2025-06-14 21:04:36 +0530

    Added Payment screen

--- App.tsx
+++ App.tsx
@@ -2,13 +2,14 @@ import { StyleSheet, View } from "react-native";
 import IntroScreens from "./src/screens/IntroScreens";
 import ContactUsScreen from "./src/screens/ContactUsScreen";
 import HomeScreen from "./src/screens/HomeScreen";
-
+import PaymentScreen from "./src/screens/PaymentScreen";
 export default function App() {
   return (
     <View>
       {/* <IntroScreens />
       <ContactUsScreen /> */}
-      <HomeScreen />
+      {/* <HomeScreen /> */}
+      <PaymentScreen />
     </View>
   );
 }

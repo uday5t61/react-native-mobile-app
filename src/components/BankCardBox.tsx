c519a9179d6b7a6cb0de04a463d00600e2bae52e
uday5t61 <uday5t61@gmail.com>
2025-06-16 11:08:31 +0530

    Added animation for bank card

--- src/components/BankCardBox.tsx
+++ src/components/BankCardBox.tsx
@@ -2,13 +2,23 @@ import { Dimensions, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { s, vs } from "react-native-size-matters";
 import { CardIcon } from "../assets/Icons";
+import LottieView from "lottie-react-native";
 
-const PHONE_WIDTH = Dimensions.get("window").width;
-const BANK_CARD_WIDTH = PHONE_WIDTH - s(24) * 2;
 const BankCardBox = () => {
   return (
     <View style={styles.bankCard}>
-      <CardIcon />
+      {/* <CardIcon /> */}
+      <LottieView
+        autoPlay
+        // ref={animation}
+        style={{
+          width: s(150),
+          height: vs(150),
+          //backgroundColor: "#eee",
+        }}
+        // Find more Lottie files at https://lottiefiles.com/featured
+        source={require("../assets/animations/BankCardAnimation.json")}
+      />
       <Text style={styles.noCardText}>No master card added</Text>
       <Text style={styles.addCardSuggestionText}>
         You can add a mastercard and save it for later
@@ -26,7 +36,8 @@ const styles = StyleSheet.create({
     borderRadius: s(10),
     alignItems: "center",
     justifyContent: "center",
-    paddingVertical: vs(35),
+    paddingTop: vs(5),
+    paddingBottom: vs(35),
     paddingHorizontal: s(30),
   },
   noCardText: {
